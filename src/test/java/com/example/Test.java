1e70ac19a1aeb488d4072956e9965913e3fb9184
zxc705505431 <705505431@qq.com>
2020-04-02 23:47:32 +0800

    将redis改为starter依赖进来

--- src/test/java/com/example/Test.java
+++ src/test/java/com/example/Test.java
@@ -1,15 +1,19 @@
 package com.example;
 
-import com.example.common.redis.RedisService;
+//import com.example.common.redis.RedisService;
+//import org.example.ratelimit.redis.RedisService;
+import com.zxc.ratelimit.redis.RedisService;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 
 /**
- * @program: RuoYi
+ * @program:
  * @description:
  * @author: Mr.Zhu
  * @create: 2019-07-10 09:46
@@ -18,19 +22,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class Test {
 
-    @Autowired
+    @Resource
     RedisService redisService;
 
-    @Autowired
-    RedisTemplate redisTemplate;
+//    @Autowired
+//    RedisTemplate redisTemplate;
 
 
     @org.junit.Test
     public void redisTest() {
         redisService.setValue("key", "hello");
-        redisTemplate.opsForValue().set("key1", "hello");
-        Object o = redisTemplate.opsForValue().get("key1");
-        System.out.println(o);
+//        redisTemplate.opsForValue().set("key1", "hello");
+//        Object o = redisTemplate.opsForValue().get("key1");
+        System.out.println(redisService.getValue("key"));
 
     }
 
