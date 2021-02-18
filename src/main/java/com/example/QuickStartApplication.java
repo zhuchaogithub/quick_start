89952d8a453bd1cf89f6d8888cefcb473ba20ac8
zxc <zxc@12.com>
2021-02-18 16:20:07 +0800

    优化

--- src/main/java/com/example/QuickStartApplication.java
+++ src/main/java/com/example/QuickStartApplication.java
@@ -1,17 +1,20 @@
 package com.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.util.StopWatch;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author  zxc
  * @date  2019/08/16
  */
+@Slf4j
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @MapperScan("com.example.*.dao")
 @EnableSwagger2
@@ -19,7 +22,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class QuickStartApplication {
 
 	public static void main(String[] args) {
+		//统计启动耗时
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start("run");
 		SpringApplication.run(QuickStartApplication.class, args);
+		stopWatch.stop();
+		log.info("run end！ {}", stopWatch.prettyPrint());
 	}
 
 }
--- src/main/java/com/example/web/controller/ProductController.java
+++ src/main/java/com/example/web/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author zxc
@@ -46,6 +47,32 @@ public class ProductController {
     public String add(@RequestBody Product product){
         productService.insert(product);
         return "SUCCESS";
+    }
+
+    @ApiOperation(value="超时处理异常", notes = "超时处理异常")
+    @PostMapping("/timeoutDealException")
+    @ResponseBody
+    public void addAndDealException(){
+        final ExecutorService exec = Executors.newFixedThreadPool(1);
+        Future<String> future = exec.submit(new Callable<String>() {
+            @Override
+            public String call() {
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    System.out.println("任务被中断。");
+                }
+                return  "OK";
+            }
+        });
+        try {
+            String result = future.get(2, TimeUnit.SECONDS);
+        } catch (InterruptedException |ExecutionException | TimeoutException e) {
+            future.cancel(true);
+            System.out.println("任务超时。");
+        }finally {
+            System.out.println("清理资源。");
+        }
     }
 
 }
